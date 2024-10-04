ac600d768ca4a5516cda6c98a54d40e3512a80b4
Naufal Faqih <113101277+naufalFaqih@users.noreply.github.com>
2024-10-04 17:08:25 +0000

    final

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,151 +1,107 @@
-import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.set_page_config(
-    page_title='GDP dashboard',
-    page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
+sns.set(style='dark')
+
+data = pd.read_csv("https://raw.githubusercontent.com/naufalFaqih/dashboard/refs/heads/master/day.csv")
+
+st.title("Bike Rental Dashboard :sparkles:")
+st.subheader('Daily Rental')
+
+col1,col2 = st.columns(2)
+with col1:
+    working_day_rent = data[data['workingday'] == 1]
+    total_rent = working_day_rent['cnt'].sum()
+    st.metric("total rental at working day", value=total_rent)
+    
+with col2:
+    holiday_rent = data[data['holiday'] == 1]
+    total_rent_holiday = holiday_rent['cnt'].sum()
+    st.metric("total rental at holiday", value = total_rent_holiday)
+
+data_ten_day = data.head(10)
+fig, ax = plt.subplots(figsize=(16,8))
+ax.plot(
+    data_ten_day['dteday'],
+    data_ten_day['cnt'],
+    marker='o',
+    linewidth = 2,
+    color="#90CAF9"
 )
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ 
+st.pyplot(fig)
 
-# -----------------------------------------------------------------------------
-# Declare some useful functions.
+st.subheader("Casual vs Registered at working day")
+col1,col2 = st.columns(2)
+with col1:
+    working_day_casual = data[data['workingday'] == 1]
+    total_rent = working_day_casual['casual'].sum()
+    st.metric("total casual at working day", value=total_rent)
+    
+with col2:
+    working_day_registered = data[data['workingday'] == 1]
+    total_rent_registered = working_day_registered['registered'].sum()
+    st.metric("total registered at working day", value = total_rent_registered)
 
-@st.cache_data
-def get_gdp_data():
-    """Grab GDP data from a CSV file.
+workingday_data = data[data['workingday'] == 1]
 
-    This uses caching to avoid having to read the file every time. If we were
-    reading from an HTTP endpoint instead of a file, it's a good idea to set
-    a maximum age to the cache with the TTL argument: @st.cache_data(ttl='1d')
-    """
+# Hitung total penyewaan casual dan registered pada hari kerja
+total_casual = workingday_data['casual'].sum()
+total_registered = workingday_data['registered'].sum()
 
-    # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+# Data untuk visualisasi
+categories = ['Casual', 'Registered']
+values = [total_casual, total_registered]
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+# Membuat plot bar
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.bar(categories, values, color=["#D3D3D3", '#FFB74D'])
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
+# Menambahkan label dan judul
+ax.set_title("Total Rent At Working day", fontsize=18)
+ax.set_ylabel(None)
 
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
+# Menampilkan plot pada Streamlit
+st.pyplot(fig)
 
-    return gdp_df
+st.subheader("Casual vs Registered at holiday")
+col1,col2 = st.columns(2)
+with col1:
+    working_day_casual = data[data['holiday'] == 1]
+    total_rent = working_day_casual['casual'].sum()
+    st.metric("total casual at holiday", value=total_rent)
+    
+with col2:
+    working_day_registered = data[data['holiday'] == 1]
+    total_rent_registered = working_day_registered['registered'].sum()
+    st.metric("total registered at holiday", value = total_rent_registered)
 
-gdp_df = get_gdp_data()
+holiday_data = data[data['holiday'] == 1]
 
-# -----------------------------------------------------------------------------
-# Draw the actual page
+# Hitung total penyewaan casual dan registered pada hari kerja
+total_casual = holiday_data['casual'].sum()
+total_registered = holiday_data['registered'].sum()
 
-# Set the title that appears at the top of the page.
-'''
-# :earth_americas: GDP dashboard
+# Data untuk visualisasi
+categories = ['Casual', 'Registered']
+values = [total_casual, total_registered]
 
-Browse GDP data from the [World Bank Open Data](https://data.worldbank.org/) website. As you'll
-notice, the data only goes to 2022 right now, and datapoints for certain years are often missing.
-But it's otherwise a great (and did I mention _free_?) source of data.
-'''
+# Membuat plot bar
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.bar(categories, values, color=["#D3D3D3", '#FFB74D'])
 
-# Add some spacing
-''
-''
+# Menambahkan label dan judul
+ax.set_title("Total Rent At Holiday", fontsize=18)
+ax.set_ylabel(None)
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+# Menampilkan plot pada Streamlit
+st.pyplot(fig)
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
-
-countries = gdp_df['Country Code'].unique()
-
-if not len(countries):
-    st.warning("Select at least one country")
-
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
-
-''
-''
-''
-
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
-
-st.header('GDP over time', divider='gray')
-
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
-
-''
-
-cols = st.columns(4)
-
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
-
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
-
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+st.subheader('Effect of Weather Conditions on Bike Rentals')
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.heatmap(data[['temp', 'atemp', 'hum', 'windspeed', 'cnt']].corr(), annot=True, ax=ax)
+st.pyplot(fig)
